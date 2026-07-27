82fee50b6f1f052beb4f040052208ff316559cb0
Veni Vidi Vici <andrzej.kuropatwinski@gmail.com>
2026-07-27 06:25:28 +0200

    plotting lib draft

--- marketsim/plot/simple_plot.py
+++ marketsim/plot/simple_plot.py
@@ -1,6 +1,20 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def simple_plot(x: list, y: list, output_file: str) -> None:
     plt.plot(x, y)
     plt.savefig(output_file)
 
+class Plotter:
+    def line(self, x: list, y: list, output_file: str) -> None:
+        plt.plot(x, y)
+        plt.savefig(output_file)
+
+    def candles(self, df: pd.DataFrame, output_file: str) -> None:
+        pass
+
+    def scatter(self, df: pd.DataFrame, output_file: str) -> None:
+        pass
+
+    def hist(self, df: pd.DataFrame, output_file: str) -> None:
+        pass
